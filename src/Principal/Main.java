91749c05dc00de13d0342a0fa248b589239949f6
Millagi <marcelo.osandon@alumnos.ucn.cl>
2026-06-13 17:47:15 -0400

    Aqui el orden, en los talleres es así el orden

---
+++ src/Principal/Main.java
@@ -0,0 +1,15 @@
+package Principal;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame menu = new JFrame("Sobres Pokemon");
+		menu.setLayout(new BorderLayout());
+		menu.setSize(500, 500);
+		menu.setVisible(true);
+	}
+}
