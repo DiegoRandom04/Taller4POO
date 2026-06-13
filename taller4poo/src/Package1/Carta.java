89612116e81f5083e50b51c877133d2654690c1a
DiegoRandom04 <138171349+DiegoRandom04@users.noreply.github.com>
2026-06-13 17:30:50 -0400

    Add files via upload

---
+++ taller4poo/src/Package1/Carta.java
@@ -0,0 +1,57 @@
+package Package1;
+
+public class Carta {
+	private String nombre;
+	private float rareza;
+	private String tipo;
+	private float poder;
+	
+	public float getPoder() {
+		return poder;
+	}
+	public Carta (String nombre, float rareza, String tipo) {
+		this.nombre = nombre;
+		this.rareza = rareza;
+		this.tipo = tipo;
+	}
+	public String getNombre() {
+		/* @return String
+		 * se usa para obtener el nombre
+		 */
+		
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		/*@ return void
+		 *  se usa para reasignar el nombre
+		 */
+		this.nombre = nombre;
+	}
+	public float getRareza() {
+		/* @ return float
+		 * se usa para obtener rareza
+		 */
+		return rareza;
+	}
+	public void setRareza(float rareza) {
+		/* @return void
+		 * se usa para reasignara rareza;
+		 */
+		
+		this.rareza = rareza;
+	}
+	public String getTipo() {
+		/*@return String
+		 * se usa para obtener el tipo
+		 */
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		/* @return void
+		 * se usa para reasignar el tipo
+		 */
+		this.tipo = tipo;
+	}
+	
+	
+}
---
+++ taller4poo/src/Package1/Pokemon.java
@@ -0,0 +1,29 @@
+package Package1;
+
+public class Pokemon extends Carta{
+	private float daño;
+	private float cantEnergias;
+	private float poder;
+	public Pokemon(String nombre, float rareza, String tipo, float daño, float cantEnergias) {
+		/* @param nombre es el nombre de la carta y se le asigna a la clase padre Carta
+		 * @param rareza es la rareza de la carta y se le asigna a la clase padre Carta
+		 * @param tipo es el tipo de carta y se le asigna a la clase padre Carta
+		 * @param daño es el daño de este tipo de carta pokemon
+		 * @param cantEnergias es otro parametro a utilzar para el calculo de poder de esta carta
+		 */
+		super(nombre,rareza,tipo);
+		
+		this.daño = daño;
+		this.cantEnergias = cantEnergias;
+	}
+	public void calcularPuntaje () {
+		/* @return void 
+		 * calcularPuntaje no tiene parametros de entrada ya que son los que pertenecen a la misma instancia
+		 * para calcular el puntaje en el caso de pokemon se usa (daño/cantEnergias)*100 segun lo informado
+		 * en la descripción del taller. en caso de otros tipos tendran otro metodo de calcular puntaje
+		 */
+		this.poder = (daño/cantEnergias)*100; 
+	}
+	
+
+}
